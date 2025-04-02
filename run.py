e9a371e63a570d8a247985aff4251be06bf9814b
louske13 <muzuoolivi.fabien@hotmail.fr>
2025-04-02 13:57:25 +0200

    Fix: auto create DB tables with db.create_all()

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
-from app import create_app
+from app import create_app, db
+from app.models import User
 
 app = create_app()
+
+with app.app_context():
+    db.create_all()  # ✅ Crée les tables s'il n'y en a pas
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000, debug=True)
